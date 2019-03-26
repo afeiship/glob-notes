014fdf56ef3054f1f0c17bd7d3bfb19df0a839af
afeiship <1290657123@qq.com>
2019-03-27 00:44:02 +0800

    fix: .vuepress glob

--- src/node-glob.js
+++ src/node-glob.js
@@ -1,4 +1,4 @@
 var glob = require('glob');
-var files = glob.sync('{**,**/.*,.*}', { ignore: '{node_modules/**,.git}' });
+var files = glob.sync('{**,.*,**/.*/*}', { ignore: '{node_modules/**,.git}' });
 
 console.log(files);
